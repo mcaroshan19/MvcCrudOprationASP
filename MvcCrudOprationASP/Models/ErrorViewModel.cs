895628d84e7070ce2edc99307f2123191f3f4066
Roshan <mcaroshan2@gmail.com>
2024-01-08 02:29:52 +0530

    Add project files.

---
+++ MvcCrudOprationASP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcCrudOprationASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
